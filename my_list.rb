8a43e116d21b2952f844db0e9fc4f21d9399cf51
yasinabdmahmood <abd181103@gmail.com>
2022-11-03 16:54:12 +0300

    Fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -10,9 +10,9 @@ class MyList
   end
 end
 
-list = MyList.new(1, 2, 3, 4)
-puts list.all? { |e| e < 5 }
-puts list.all? { |e| e > 5 }
-puts list.any? { |e| e == 2 }
-puts list.any? { |e| e == 5 }
-puts list.filter &:even?
+# list = MyList.new(1, 2, 3, 4)
+# puts list.all? { |e| e < 5 }
+# puts list.all? { |e| e > 5 }
+# puts list.any? { |e| e == 2 }
+# puts list.any? { |e| e == 5 }
+# puts list.filter &:even?
