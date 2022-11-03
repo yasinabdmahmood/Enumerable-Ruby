d5e7d7f8131bef20446112eeafa8d3b9143ac364
yasinabdmahmood <abd181103@gmail.com>
2022-11-03 16:34:08 +0300

    Create MyList class

---
+++ myList.rb
@@ -0,0 +1,18 @@
+require_relative 'MyEnumerable'
+class MyList
+  def initialize(*list)
+    @list = list
+  end
+
+  include MyEnumerable
+  def each
+    @list.each { |value| yield value if block_given? }
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+puts list.all? { |e| e < 5 }
+puts list.all? { |e| e > 5 }
+puts list.any? { |e| e == 2 }
+puts list.any? { |e| e == 5 }
+puts list.filter &:even?
